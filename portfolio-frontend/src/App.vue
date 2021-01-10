3a30ab076e44489852a384c91c0f4ef29ce84534
Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>
2021-01-10 16:37:35 -0400

    Made changes
    Signed-off-by: Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>

--- portfolio-frontend/src/App.vue
+++ portfolio-frontend/src/App.vue
@@ -1,14 +1,10 @@
 <template>
-  <NavigationBar />
   <router-view />
 </template>
 
 <script>
-import NavigationBar from './components/NavigationBar';
 export default {
-  components: {
-    NavigationBar
-  }
+
 }
 </script>
 
