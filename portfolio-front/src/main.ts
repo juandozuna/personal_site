4382451cf77af6a504a2c4e5a0e66706124c8da5
Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>
2021-07-20 12:11:02 -0400

    Removed front end in vuejs 3
    Signed-off-by: Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>

--- portfolio-front/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.scss';
-
-createApp(App).use(router).mount('#app')
--- portfolio-front/src/views/Welcome.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-blue-400">
-
-  </div>
-</template>
